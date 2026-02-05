d6458e64cc5091425bd87dcd29c3bdbe2fd86b6c
Shubhanshi Chaudhary <shubhanshi1105@gmail.com>
2026-02-06 01:20:08 +0530

    Add Manga page structure and daily updates section

--- src/pages/Manga.jsx
+++ src/pages/Manga.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import MangaList from "../components/manga/MangaList";
+import DailyUpdates from "../components/manga/DailyUpdates";
+
 
 export default function Manga() {
   return (
-    <div>Manga</div>
-  )
+    <div className="px-6 pt-10">
+      <MangaList />
+      <DailyUpdates/>
+    </div>
+  );
 }
+
