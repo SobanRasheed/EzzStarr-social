9dda5a6267c9ee3a2bcbce6c11580933acc37ae9
Harsh Upadhyay <harsh9756416665@gmail.com>
2026-02-20 14:26:32 +0530

    added a loader

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./App";
+import Loader from "./Loader";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Suspense fallback={<div className="fixed inset-0 flex items-center justify-center bg-black text-white">Loading...</div>}>
+    <Suspense fallback={<div className="fixed inset-0 flex items-center justify-center bg-black text-white"><Loader /></div>}>
       <RouterProvider router={router} />
     </Suspense>
   </React.StrictMode>
